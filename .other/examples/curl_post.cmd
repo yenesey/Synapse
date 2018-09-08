e32e5fd9284d9c1eb084668cc3927b07d8d4ea75
yenesey <denis-ter@mail.ru>
2018-09-08 21:54:14 +0300

    Update curl_post.cmd

--- .other/examples/curl_post.cmd
+++ .other/examples/curl_post.cmd
@@ -1,16 +1,16 @@
 set url=https://kr-ws-tech-02:444/tasks?id=39
 set curl=..\XTECH\tools\curl
-set cred=ifcbank\bogachev_di:****
+set cred=bank\bogachev_di:****
 set post=%curl% -s --insecure -i --user %cred% --ntlm
 rem %put% "{\"$db\":\"tst.db\", \"$table\":\"finrez\" , \"$sql\":\"update finrez set depkoracc=1\"}" %url%
 
 rem -H "Content-Type:multipart/form-data;charset=utf-8" 
-rem -F "file128=@D:\«¥¬¥­βλ ο§λª  SQL.docx"
+rem -F "file128=@D:\ΒΒ«Β¥Β¬Β¥Β­ΓΆΓ« Γ―Β§Γ«ΒªΒ  SQL.docx"
 rem -X POST -H "Content-Type: text/html; charset=UTF-8" --data-ascii
 
 
 rem set post=%curl% -X POST -H "Content-Type:text/html;charset=UTF-8" -s --insecure -i --user %cred% --ntlm
-%post% -F "file128=@C:\α θ .jpg" %url%
+%post% -F "file128=@C:\Γ΅Β Γ¨Β .jpg" %url%
 
 
 
