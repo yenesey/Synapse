f69b9c08d9c1317d23bd4cd9ee5846b94fe2a9d0
Denis Bogachev <denesey@gmail.com>
2019-09-22 22:35:33 +0300

    config async workaround

--- core/api/config.js
+++ core/api/config.js
@@ -5,79 +5,88 @@
 
 */
 
-const deepProxy = require('../deep-proxy')
-
 module.exports = function (system) {
 	//
-	function buildNode (_id = null) { // well... brainfu..ng, but works :)
-		let node = {}
-		return system.db('select * from config where id_p ' + (_id == null ? 'is null' : '=' + String(_id)))
+
+	function addNode (target, key, value, _id) {
+		if (Reflect.set(target, key, value)) {
+			save(_id, key, value).then(newId => {
+				if (typeof value === 'object') {
+					value = proxify(value, newId)
+				}
+				for (let k in value) {
+					addNode(target[key], k, value[k], newId)	
+				}
+			}).catch(console.log)
+			return true
+		}
+		return false
+	}
+
+	function proxify (node, _id) { 
+		// console.log('proxify ', node, ' ', _id)
+		return new Proxy(node, {
+			set (target, key, value, receiver) { // about target[key] := value
+				return addNode(target, key, value, _id)
+			},
+
+			deleteProperty (target, key) {
+				// console.log('del id_p=', _id, '    ', key)
+				if (Reflect.has(target, key)) {
+					Reflect.deleteProperty(target, key)
+					del(_id, key)
+					return true
+				}
+				return false
+			}
+		})
+	}
+
+	function buildNode (_id = -1) { // well... brainfu..ng, but works :)
+		let node = {} // Object.create(null)
+		return system.db('select * from config where id_p = ? and id != id_p', [_id])
 			.then(children => {
 				if (children.length > 0) {
 					return children.reduce((p, child) =>
-						p.then(() => buildNode(child.id).then(childNode => { // childNode may be a single value or other node just built
-							node[child.key] = childNode
-							return node
-						}))
+						p.then(() => 
+							buildNode(child.id)
+								.then(childNode => {
+									node[child.key] = childNode
+								})
+						)
 					, Promise.resolve(null)
-					)
+					).then(() => proxify(node, _id))
 				} else { // no children means we're at the 'leaf'
-					return system.db('select * from config where id = ?', [_id]).then(select => select[0].value)
+					return system.db('select * from config where id = ?', [_id])
+						.then(select => select[0].value)
 				}
 			})
 	}
 
-	function pathToId (node, path) {
-		return path.reduce((p, key) =>
-			p.then((_id) => {
-				return system.db('select * from config where id_p ' + (_id == null ? 'is null' : '=' + String(_id)) + ' and key = "' + key + '"')
-					.then(select => {
-						if (select.length) return select[0].id
-						return null
-					})
-			})
-		, Promise.resolve(null)
-		)
+	function save (id_p, key, value = null) {
+		return system.db('replace into config (id_p, [key], value) values ($id_p, $key, $value)', { $id_p: id_p, $key: key, $value: value })
 	}
 
-	function saveValue (node, path, value) {
-		return pathToId(node, path)
-			.then(_id => {
-				if (_id) {
-					system.db('update config set value = $value where id = $id', { $id: _id, $value: value })
-				} else {
-					let key = path.pop()
-					pathToId(node, path).then(_id => {
-						if (_id) {
-							system.db('insert into config (id_p, [key], value) values ($id_p, $key, $value)', { $id_p: _id, $key: key, $value: value })
-						}
-					})
-				}
-			})
-	}
-
-	function deleteValue (node, path) {
-		return pathToId(node, path)
-			.then(_id => system.db('delete from config where id = $id', { $id: _id }))
+	function del (id_p, key) {
+		return system.db('delete from config where id_p = $id_p and key = $key', { $id_p: id_p, $key: key })
 	}
 
 	var config = null
-	buildNode().then(node => {
-		config = deepProxy(node, {
-			set (target, path, value, receiver) {
-				saveValue(config, path, value)
-			},
-			deleteProperty (target, path) {
-				deleteValue(config, path)
-			}
-		})
+	buildNode().then(node => { 
+		config = node
+
+		//node.admin = {}
+		//config.admin.panel = true
+		console.log(node)
 	})
 
+	// -------------------------------------------------------------------------------------------
 	function getNode (path) {
 		let node = config
 		let level = 0
+		if (config == null) return null
 		while (level < path.length) {
-			if (path[level] in node) node = node[path[level]]; else return node
+			if (path[level] !== '') node = node[path[level]]
 			level++
 		}
 		return node
@@ -85,6 +94,8 @@ module.exports = function (system) {
 
 	this.get('*', function (req, res) {
 		let path = req.url.substring(1).split('/')
+	
+		/*
 		let last = path.slice(-1).pop()
 		let node = null
 		if (last.indexOf('=') !== -1) {
@@ -93,18 +104,15 @@ module.exports = function (system) {
 			node = getNode(path)
 			// todo: разобраться с созданием нод!
 			// нода с пустым value должна иметь возможность стать контейнером без ругательств!!!
-			if (value === '..' && (key in node) && typeof node[key] !== 'object') {
-				node[key] = {}
-			} else {
-				node[key] = value
-			}
+			node[key] = value
 		} else if (last.charAt(last.length - 1) === '!') {
 			path.pop()
 			let [key, value] = last.split('!')
 			node = getNode(path)
 			delete node[key]
 		}
-		node = getNode(path)
+		*/
+		let node = getNode(path)
 		res.json(node)
 	})
 }
